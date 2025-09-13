f3cf52ad1bab59ff2191903724ee77428a0c60ef
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-09-13 23:08:16 +0530

    9/9

--- client/src/components/Doctors.jsx
+++ client/src/components/Doctors.jsx
@@ -58,6 +58,7 @@ const Doctors = () => {
                 </div>
                 <h1>hi</h1>
                  <h1>hi</h1>
+                  <h1>hi</h1>
               </div>
             ))}
           </div>
